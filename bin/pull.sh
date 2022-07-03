642d4a584ae9ab73893f64fac9a233af3c7fc97a
jaemin an <jaeminan@jaeminui-Macmini.local>
2022-07-03 22:56:55 +0900

    Feat : update script

--- bin/pull.sh
+++ bin/pull.sh
@@ -6,10 +6,10 @@ cd team1-imageboard
 git fetch --all
 git reset --hard origin/develop
 git pull origin develop
-../gradlew build -x check
+./gradlew build -x check
 cd ~/../../var/lib/tomcat9/webapps
 sudo rm -rf ROOT
 sudo rm -rf ROOT.war
 cd ~/team1-imageboard/build/libs
 sudo mv imageBoard.war ~/../../var/lib/tomcat9/webapps/ROOT.war
-sudo systemctl restart togimcat9
+sudo systemctl restart tomcat9
