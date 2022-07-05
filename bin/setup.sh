0632fa8a25e8295c60bfe11f85d15a1b00059518
anGit <55350092+anjm1020@users.noreply.github.com>
2022-07-05 17:15:18 +0900

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,15 +2,15 @@
 # setup.sh
 # @param $1=db_url, $2=db_username, $3=db_password, $4=jwt_secret
 
-echo ===========================================
-echo 1. Install Dependency
-echo ===========================================
+echo "==========================================="
+echo "1. Install Dependency"
+echo "==========================================="
 sudo apt-get update
 sudo apt-get install default-jdk xmlstarlet tomcat9 git -y
 
-echo ===========================================
-echo 2. Repository Check, Git Configure, Pull
-echo ===========================================
+echo "==========================================="
+echo "2. Repository Check, Git Configure, Pull"
+echo "==========================================="
 git config --global user.email anjm1020@gmail.com
 git config --global user.name jaemin
 cd ~
@@ -20,9 +20,9 @@ git fetch --all
 git reset --hard origin/develop
 git pull origin develop
 
-echo ===========================================
+echo "==========================================="
 echo 3. Make Spring Configuration file
-echo ===========================================
+echo "==========================================="
 cd ~/team1-imageboard/src/main/resources/
 [ -f application.properties ] && rm application.properties
 sudo echo spring.datasource.url=$1 >> application.properties
@@ -31,34 +31,35 @@ sudo echo spring.datasource.password=$3 >> application.properties
 sudo echo jwt.secret=$4 >> application.properties
 cat application.properties
 
-echo ===========================================
-echo 4. Build
-echo ===========================================
+echo "==========================================="
+echo "4. Build"
+echo "==========================================="
 cd ~/team1-imageboard/
 ./gradlew build -x check
 
-echo ===========================================
-echo 5. Set war file to Tomcat
-echo ===========================================
+echo "==========================================="
+echo "5. Set war file to Tomcat"
+echo "==========================================="
 cd ~/../../var/lib/tomcat9/webapps
 sudo rm -rf ROOT
 sudo rm -rf ROOT.war
 cd ~/team1-imageboard/build/libs
 sudo mv imageBoard.war ~/../../var/lib/tomcat9/webapps/ROOT.war
 cd ~/../../var/lib/tomcat9/webapps/
-echo Directory : webapps ls
+echo "Directory : tomcat9/webapps"
 ls
 
-echo ===========================================
-echo 6. Set Tomcat Port
-echo ===========================================
+echo "==========================================="
+echo "6. Set Tomcat Port"
+echo "==========================================="
 cd ~/../../etc/tomcat9
 sudo xmlstarlet ed --inplace --update "/Server/Service/Connector/@port" -v 80 server.xml
-echo Tomcat Port
+echo "Tomcat Port"
 sudo xmlstarlet sel -T -t -v "/Server/Service/Connector/@port" server.xml
 echo
 
-echo 7. Tomcat Restart
-echo ===========================================
+echo "==========================================="
+echo "7. Tomcat Restart"
+echo "==========================================="
 sudo systemctl restart tomcat9
-echo Success - Tomcat Restart
+echo "Success - Tomcat Restart"
