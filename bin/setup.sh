020968a6415820379c662538059dddcbc2458561
jaemin <jaemin@jaeminui-MacBookPro.local>
2022-07-05 09:29:54 +0900

    Feat : update script file

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,35 +2,52 @@
 # setup.sh
 # @param $1=db_url, $2=db_username, $3=db_password, $4=jwt_secret
 
-# apt update, install dependency
+echo ###############################################
+echo 1. Install Dependency
+echo ###############################################
 sudo apt-get update
 sudo apt-get install default-jdk xmlstarlet tomcat9 git -y
 
-# set config and build
+echo ###############################################
+echo 2. Git Configure, Pull
+echo ###############################################
 git config --global user.email anjm1020@gmail.com
 git config --global user.name jaemin
+cd ~/team1-imageboard/
+git fetch --all
+git reset --hard origin/develop
 git pull origin develop
-cd
-cd team1-imageboard/bin/
-sudo chmod 755 *.sh
-cd
-cd team1-imageboard/src/main/resources/
+
+echo ###############################################
+echo 3. Make Spring Configuration file
+echo ###############################################
+cd ~/team1-imageboard/src/main/resources/
 sudo echo spring.datasource.url=$1 >> application.properties
 sudo echo spring.datasource.username=$2 >> application.properties
 sudo echo spring.datasource.password=$3 >> application.properties
 sudo echo jwt.secret=$4 >> application.properties
+
+echo ###############################################
+echo 4. Build
+echo ###############################################
 ./gradlew build -x check
 
-# place war file to tomcat directory
+echo ###############################################
+echo 5. Set war file to Tomcat
+echo ###############################################
 cd ~/../../var/lib/tomcat9/webapps
 sudo rm -rf ROOT
 sudo rm -rf ROOT.war
 cd ~/team1-imageboard/build/libs
 sudo mv imageBoard.war ~/../../var/lib/tomcat9/webapps/ROOT.war
 
-# configure tomcat port
+echo ###############################################
+echo 6. Set Tomcat Port
+echo ###############################################
 cd ~/../../etc/tomcat9
 sudo xmlstarlet ed --inplace --update "/Server/Service/Connector/@port" -v 80 server.xml
 
-# tomcat start
+echo ###############################################
+echo 7. Tomcat Restart
+echo ###############################################
 sudo systemctl restart tomcat9
