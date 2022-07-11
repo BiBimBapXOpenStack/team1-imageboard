15314f52d6ed90e4629920ff279e0dcb5bbbcc97
jaemin <anjm1020@gmail.com>
2022-07-11 20:34:05 +0900

    Feat : 스크립트 업데이트

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,9 @@
 # setup.sh
 # @param $1=db_url, $2=db_username, $3=db_password, $4=jwt_secret
 
+repoDir=/home/ubuntu/team1-imageboard
+tomcatWebappsDir=/var/lib/tomcat9/webapps
+
 echo "==========================================="
 echo "1. Install Dependency"
 echo "==========================================="
@@ -11,11 +14,8 @@ sudo apt-get install default-jdk xmlstarlet tomcat9 git -y
 echo "==========================================="
 echo "2. Repository Check, Git Configure, Pull"
 echo "==========================================="
-git config --global user.email anjm1020@gmail.com
-git config --global user.name jaemin
-cd ~
 [ -d team1-imageboard ] || git clone https://github.com/BiBimBapXOpenStack/team1-imageboard.git 
-cd ~/team1-imageboard/
+cd $repoDir
 git fetch --all
 git reset --hard origin/develop
 git pull origin develop
@@ -23,7 +23,7 @@ git pull origin develop
 echo "==========================================="
 echo 3. Make Spring Configuration file
 echo "==========================================="
-cd ~/team1-imageboard/src/main/resources/
+cd $repoDir/src/main/resources/
 [ -f application.properties ] && rm application.properties
 sudo echo spring.datasource.url=$1 >> application.properties
 sudo echo spring.datasource.username=$2 >> application.properties
@@ -34,25 +34,25 @@ cat application.properties
 echo "==========================================="
 echo "4. Build"
 echo "==========================================="
-cd ~/team1-imageboard/
+cd $repoDir
 ./gradlew build -x check
 
 echo "==========================================="
 echo "5. Set war file to Tomcat"
 echo "==========================================="
-cd ~/../../var/lib/tomcat9/webapps
+cd $tomcatWebappsDir
 sudo rm -rf ROOT
 sudo rm -rf ROOT.war
-cd ~/team1-imageboard/build/libs
-sudo mv imageBoard.war ~/../../var/lib/tomcat9/webapps/ROOT.war
-cd ~/../../var/lib/tomcat9/webapps/
+cd $repoDir/build/libs
+sudo mv imageBoard.war $tomcatWebappsDir/ROOT.war
+cd $tomcatWebappsDir
 echo "Directory : tomcat9/webapps"
 ls
 
 echo "==========================================="
 echo "6. Set Tomcat Port"
 echo "==========================================="
-cd ~/../../etc/tomcat9
+cd /etc/tomcat9
 sudo xmlstarlet ed --inplace --update "/Server/Service/Connector/@port" -v 80 server.xml
 echo "Tomcat Port"
 sudo xmlstarlet sel -T -t -v "/Server/Service/Connector/@port" server.xml
