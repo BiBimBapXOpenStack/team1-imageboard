71f4925936131f80e698943a6d60007ff61d780e
jaemin <anjm1020@gmail.com>
2022-07-12 15:23:53 +0900

    Refactor : 테스트 컨트롤러 패키지 이동

--- src/main/java/bibimbap/openstack/imageboard/controller/test/TestController.java
+++ src/main/java/bibimbap/openstack/imageboard/controller/test/TestController.java
@@ -1,4 +1,4 @@
-package bibimbap.openstack.imageboard.controller;
+package bibimbap.openstack.imageboard.controller.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
